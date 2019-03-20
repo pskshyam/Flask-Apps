a7f0ffcbee3748b8f65f2f4c5b83af3920ab6d95
Shyam <shyam.psk@gmail.com>
2019-03-20 17:56:42 +0530

    Project files added

---
+++ ModelDeploymentUsingFlask&Pipeline/server/main.py
@@ -0,0 +1,47 @@
+import logging
+import dill
+import pandas as pd
+from flask import Flask, request
+from werkzeug.exceptions import BadRequest
+
+application = Flask(__name__)
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
+
+def build_model(model_path):
+    with open(model_path, "rb") as f:
+        mdl = dill.load(f)
+    return mdl
+
+
+def launch_model(mdl, request):
+    try:
+        features_as_dict = request.get_json()
+        features_as_df = pd.io.json.json_normalize(features_as_dict)
+
+        logger.debug("Predicting {}".format(features_as_df))
+        binary_predictions = mdl.predict(features_as_df)
+
+        detected_zones = str(binary_predictions[0])
+    except Exception:
+        logger.exception("Error in pipeline")
+        raise BadRequest(description=e.message)
+
+    return detected_zones
+
+
+@application.route("/predict", methods=["POST"])
+def handle_predict_request():
+    global model
+    return launch_model(model, request)
+
+
+if __name__ == "__main__":
+    try:
+        model = build_model("pipeline.pk")
+        application.run()
+    except KeyboardInterrupt:
+        logger.exception("Shutting down")
+    except Exception:
+        logger.exception("Error in initialization chain")
---
+++ ModelDeploymentUsingFlask&Pipeline/training/training.py
@@ -0,0 +1,39 @@
+import dill
+from pandas import read_csv
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.preprocessing import FunctionTransformer
+from sklearn_pandas import DataFrameMapper
+
+path = "../pima-indians-diabetes.csv"
+features = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age']
+label = 'label'
+dataframe = read_csv(path, names=features + [label])
+
+X = dataframe[features]
+Y = dataframe[label]
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+
+def is_adult(x): return x > 18
+
+def is_bigger_than(x, threshold): return x > threshold
+
+clf = Pipeline([
+    ("mapper", DataFrameMapper([
+        (['age'], FunctionTransformer(is_adult)),
+        (features, None)
+    ])),
+    ("classifier", LogisticRegression())
+])
+
+# grid search
+params_grid = {'classifier__C': [0.1, 0.5, 1, 5, 10]}
+clf = GridSearchCV(clf, params_grid, cv=3, verbose=3, n_jobs=1)
+
+clf.fit(X_train, Y_train)
+print(clf.score(X_test, Y_test))
+
+with open('../server/pipeline.pk', 'wb') as f:
+    dill.dump(clf.best_estimator_, f)
